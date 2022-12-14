ec379aefb002fe42812e21c0966ed77a5d5b1581
Dmitry <dmitry91ivanov@yandex.ru>
2022-12-14 16:00:17 +0300

    Задача 29 решена (k)

--- Zadacha29.cs
+++ Zadacha29.cs
@@ -5,14 +5,14 @@
 
 6, 1, 33 -> [6, 1, 33] */
 
-void arrayOne(int [] a)
+void ArrayOne(int [] a)
 {
     for (int i = 0; i < a.Length; i++)
     {
         a[i] = new Random().Next(1,10);
     }
 }
-void printArray (int [] a)
+void PrintArray (int [] a)
 {
     Console.Write("[");
     for (int i = 0; i < a.Length; i++)
@@ -22,5 +22,5 @@ void printArray (int [] a)
     Console.Write("]");
 }
 int[] array = new int[8];
-arrayOne(array);
-printArray(array);
+ArrayOne(array);
+PrintArray(array);
